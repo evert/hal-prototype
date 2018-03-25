7b9e8bb11f8f97b19fcc858ef54211a185ea8e67
Evert Pot <me@evertpot.com>
2018-03-24 19:10:50 -0700

    CORS headers

---
+++ src/middleware/cors.ts
@@ -0,0 +1,27 @@
+import { Context } from 'koa';
+
+const allowedHeaders = [
+  'Content-Type',
+  'User-Agent',
+];
+
+const allowedOrigin = [
+  '*',
+];
+
+/**
+ * This middleware sets CORS headers to anything may call it.
+ */
+export default async function middleware(ctx: Context, next: Function) {
+  ctx.response.set('Access-Control-Allow-Headers', allowedHeaders.join(','));
+  ctx.response.set('Access-Control-Allow-Origin', allowedOrigin.join(','));
+
+  if (ctx.method === 'OPTIONS' && ctx.request.get('Origin')) {
+    // It was a preflight-request. Don't call other middlewares.
+    ctx.body = '';
+    ctx.status = 204;
+    return;
+  }
+
+  return next();
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import Koa from 'koa';
 import { Context } from 'koa';
 
+import cors from './middleware/cors';
 import resourceStore from './middleware/resource-store';
 import browser from './middleware/browser';
 import halEmbed from './middleware/hal-embed';
 
 const app = new Koa();
 
+app.use(cors);
 app.use(browser);
 app.use(halEmbed);
 app.use(resourceStore(__dirname + '/../blobs'));
